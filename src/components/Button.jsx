cab4d2a8ecd2b6b25ee2e201b86cfddf5a0e5350
karolinabodis <78912800+karolinabodis@users.noreply.github.com>
2022-04-12 15:00:20 +0100

    Making it reusable

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function Button(props) {
+export default function Button({ title, handleClick }) {
 	return (
 		<button
 			className="bg-slate-500 text-gray-200 hover:bg-slate-400 hover:-translate-y-0.5 transform transition active:bg-slate-600 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-slate-400 px-4 py-2 rounded-lg uppercase shadow-lg"
-			onClick={props.handleClick}
+			onClick={handleClick}
 		>
-			New game
+			{title}
 		</button>
 	);
 }
